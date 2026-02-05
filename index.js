db805f42f94e0eb83e779d12addcf047f7e75ff1
Mooyguy <muyiwaoladipoo@gmail.com>
2026-02-05 13:19:32 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// IMPORTANT: Use the system port variable or fallback to 3000
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello! This app is running on Elastic Beanstalk.</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
